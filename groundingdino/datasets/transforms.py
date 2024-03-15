b8d0d05b24fff8c20b273d9004d5297c11b15df1
ece <vigneshkannan1696@gmail.com>
2024-03-15 18:26:34 +0530

    fix(paths): fixed import errors according to new path

--- groundingdino/datasets/transforms.py
+++ groundingdino/datasets/transforms.py
@@ -10,8 +10,8 @@ import torch
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
 
-from groundingdino.util.box_ops import box_xyxy_to_cxcywh
-from groundingdino.util.misc import interpolate
+from submodules.GroundingDINO.groundingdino.util.box_ops import box_xyxy_to_cxcywh
+from submodules.GroundingDINO.groundingdino.util.misc import interpolate
 
 
 def crop(image, target, region):
--- groundingdino/util/inference.py
+++ groundingdino/util/inference.py
@@ -8,11 +8,11 @@ from PIL import Image
 from torchvision.ops import box_convert
 import bisect
 
-import groundingdino.datasets.transforms as T
-from groundingdino.models import build_model
-from groundingdino.util.misc import clean_state_dict
-from groundingdino.util.slconfig import SLConfig
-from groundingdino.util.utils import get_phrases_from_posmap
+import submodules.GroundingDINO.groundingdino.datasets.transforms as T
+from submodules.GroundingDINO.groundingdino.models import build_model
+from submodules.GroundingDINO.groundingdino.util.misc import clean_state_dict
+from submodules.GroundingDINO.groundingdino.util.slconfig import SLConfig
+from submodules.GroundingDINO.groundingdino.util.utils import get_phrases_from_posmap
 
 # ----------------------------------------------------------------------------------------------------------------------
 # OLD API
